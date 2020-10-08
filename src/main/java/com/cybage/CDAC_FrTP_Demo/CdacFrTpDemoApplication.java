44af32fba7cf355fe9f33d8ba9731e7314835356
Mangesh Suryawanshi <mangeshsu@cyabage.com>
2020-10-08 18:43:44 +0530

    Initial Commit

---
+++ src/main/java/com/cybage/CDAC_FrTP_Demo/CdacFrTpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cybage.CDAC_FrTP_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdacFrTpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdacFrTpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybage/CDAC_FrTP_Demo/CdacFrTpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybage.CDAC_FrTP_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdacFrTpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
